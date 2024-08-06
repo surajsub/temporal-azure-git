4687f387583e24f2ac00bdc87f7c5adc7dc4c552
surajsub <surajsub@gmail.com>
2024-08-05 22:49:00 -0500

    Update kubeconfig.go

--- activities/kubeconfig.go
+++ activities/kubeconfig.go
@@ -23,7 +23,7 @@ func (a *AKSImpl) KubeConfigActivity(ctx context.Context, aks models.AKS) (strin
 func (a *AKSImpl) DeployResourcesWithKubectlActivity(ctx context.Context, kubeconfigPath, yamlFilePath string, aks models.AKS) error {
 
 	yamlFilePath = "/tmp/aks-store-quickstart.yaml"
-	activity.GetLogger(ctx).Info("Starting the Application Deploy Work with the following values", kubeconfigPath, "and the yamlfile path is", kubeconfigPath, yamlFilePath)
+	activity.GetLogger(ctx).Info("Starting the Application Deploy Work with the following values", kubeconfigPath, "and the yamlfile path is", yamlFilePath)
 	log.Println("calling the following command ")
 	log.Printf("kubectl --kubeconfig %s apply -f %s\n", kubeconfigPath, yamlFilePath)
 	kubectlCmd := exec.Command("kubectl", "--kubeconfig", kubeconfigPath, "apply", "-f", "/tmp/aks-store-quickstart.yaml")
